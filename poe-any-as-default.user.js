f7a28aae1b184a38740994c0cc7a331929d5b3e4
D4Enjoyer <146950055+D4Enjoyer@users.noreply.github.com>
2023-11-25 10:59:04 +0100

    Update poe-any-as-default.user.js

--- poe-any-as-default.user.js
+++ poe-any-as-default.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         poe-any-as-default
 // @namespace    https://github.com/D4Enjoyer/poe-any-as-default
-// @version      1.0
+// @version      1.1
 // @description  Sets the default Sale Type to "Any" on Path of Exile Trade
 // @author       A God Gamer with his dear friend ChatGPT
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pathofexile.com
@@ -11,32 +11,36 @@
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
-(function($) {
-    'use strict';
-    
-    // Function to select "Any" option under "Sale Type" filter
-    function selectAnyOption(jNode) {
-        const title = jNode[0];
+// Function to select "Any" option under "Sale Type" filter
+function selectAnyOption(jNode) {
+    // Extract the DOM element containing the text "Sale Type"
+    const title = jNode[0];
 
-        if (title.textContent.trim() === 'Sale Type') {
-            const multiselectWrapper = title.parentElement.querySelector('.multiselect__content-wrapper');
+    // Check if the element contains "Sale Type"
+    if (title.textContent.trim() === 'Sale Type') {
+        // Find the wrapper for the multiselect dropdown
+        const multiselectWrapper = title.parentElement.querySelector('.multiselect__content-wrapper');
 
-            // Check if multiselect is present and not already activated
-            if (multiselectWrapper && !multiselectWrapper.parentElement.classList.contains('multiselect--above')) {
-                // Simulate click to activate the multiselect
-                title.click();
-                console.log('Clicked on multiselect to activate it');
+        // Check if the multiselect exists and is not active
+        if (multiselectWrapper && !multiselectWrapper.parentElement.classList.contains('multiselect--above')) {
+            // Click to activate the multiselect dropdown
+            title.click();
+            console.log('Clicked on multiselect to activate it');
 
-                // Find and click the "Any" option under "Sale Type" filter
-                const anyOption = multiselectWrapper.querySelector('.multiselect__element span[data-select=""] span');
-                if (anyOption && anyOption.textContent.trim() === 'Any') {
-                    anyOption.click();
-                    console.log('Clicked on "Any" option under "Sale Type"');
-                }
+            // Find and click the "Any" option under "Sale Type"
+            const anyOption = multiselectWrapper.querySelector('.multiselect__element span[data-select=""] span');
+            if (anyOption && anyOption.textContent.trim() === 'Any') {
+                anyOption.click();
+                console.log('Clicked on "Any" option under "Sale Type"');
             }
         }
     }
+}
 
-    // Wait for "Sale Type" element to appear and select the "Any" option
-    waitForKeyElements('.filter-title', selectAnyOption);
+// Wait for ".filter-title" that contains "Sale Type" to appear and select the "Any" option
+(function($) {
+    'use strict';
+
+    // Use waitForKeyElements utility to wait for the specific element to appear
+    waitForKeyElements('.filter-title:contains("Sale Type")', selectAnyOption);
 })(jQuery);
